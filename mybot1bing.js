762d70b807be38d01d8d79f6a5223fed16322f98
Eg0ry4 <166389917+Eg0ry4@users.noreply.github.com>
2024-05-09 02:08:59 +0300

    Доработки бинг-бота по версии 1.02
    Работает на целевом сайте, с долей вероятности уходит в поиск

--- mybot1bing.js
+++ mybot1bing.js
@@ -5,51 +5,88 @@
 // @description  Bot for Bing
 // @author       Kochetov Egor 066
 // @match        https://www.bing.com/*
+// @match        https://auto.ru/*
 // @grant        none
 // ==/UserScript==
 
 let bingInput = document.getElementsByName("q")[0];
 let bingBtn = document.getElementsByClassName("search")[0];
 let links = document.links;
-let keywords = ["Купить машину", "BMW e46 купить", "Качественные иномарки"];
+let keywords = ["Купить машину", "BMW e46 купить", "Раритетные иномарки в Москве", "Купить мотоцикл Harley-Davidson", "Купить мотоцикл типа cafe racer"];
 let keyword = keywords[getRandom(0, keywords.length)];
+let mouseClick = new MouseEvent("click");
 
 //Работаем на главной странице
 if (bingBtn !== undefined) {
   let i = 0;
+  //bingInput.focus();
+  //bingInput.dispatchEvent(mouseClick);
+  //bingInput.value = keyword;
+  //bingBtn.click();
+  // Иная вариация
   let timerId = setInterval(() => {
+    bingInput.click();
     bingInput.value += keyword[i];
     i++;
     if(i == keyword.length) {
       clearInterval(timerId);
       bingBtn.click();
     }
-  }, 500)
-  //Работаем на странице поисковой системы
-  } else if (document.querySelector(".b_scopebar") !== null) {
-    let nextPage = true;
-    for (let i = 0; i < links.length; i++) {
-      if (links[i].href.indexOf("auto.ru") != -1) {
-        let link = links[i];
-        let nextPage = false;
-        console.log("Найден результат " + link);
-        setTimeout(() => {
-          link.click();
-        }, getRandom(1500, 3000));
-        break;
-      }
-    }
-    if (document.querySelector(".sb_pagS").innerText == "10") {
-      let nextPage = false;
-      location.href = "https://www.bing.com/";
-    }
+  }, getRandom(300, 500))
+  //Работаем на целевом сайте
+  } else if (location.hostname == "auto.ru") {
+    let linkIndex = getRandom(0, links.length);
+    let localLink = links[linkIndex];
+    setInterval(() => {
 
-    if (nextPage) {
+      if (getRandom(0, 101) > 50) {
+        location.href = "https://www.bing.com/";
+      }
+
+      if (links.length == 0) {
+        location.href = "https://auto.ru/";
+      }
+
+      if (localLink.href.includes("auto.ru")) {
+        localLink.click();
+      }
+    }, getRandom(3000, 5000))
+    console.log("Мы на целевом сайте");
+  }
+//Работаем на странице поисковой системы
+else if (document.querySelector(".b_scopebar") !== null) {
+  //привязка к scopebar позволяет избежать многих ошибок с дальнейшей работой бота на выбранном ресурсе
+  let nextPage = true;
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("auto.ru") != -1) {
+      let link = links[i];
+      let nextPage = false;
+      console.log("Найден результат " + link);
       setTimeout(() => {
-        document.querySelector(".sb_pagN").click();
-      }, getRandom(1500, 3000))
+        link.click();
+      }, getRandom(2500, 4000));
+      break;
     }
   }
+  let elementExist = setInterval(() => {
+    let elem = document.querySelector(".sb_pagS");
+
+    if (elem !== null ) {
+      if (document.querySelector(".sb_pagS").innerText == "10") {
+        let nextPage = false;
+        location.href = "https://www.bing.com/";
+      }
+      clearInterval(elementExist);
+    }
+  }, 100);
+
+
+  if (nextPage) {
+    setTimeout(() => {
+      document.querySelector(".sb_pagN").click();
+    }, getRandom(3000, 4500))
+  }
+}
 function getRandom(min,max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
