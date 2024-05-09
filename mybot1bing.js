1cd9dd694e8d4986c644c97b50738d72df5e7c8a
Eg0ry4 <166389917+Eg0ry4@users.noreply.github.com>
2024-05-09 08:19:58 +0300

    Версия 1.03
    Добавлено несколько сайтов. 
    Пока не доработано...

--- mybot1bing.js
+++ mybot1bing.js
@@ -1,20 +1,28 @@
 // ==UserScript==
 // @name         My first Bot
 // @namespace    http://http://inoue.p-host.in
-// @version      1.02
+// @version      1.03
 // @description  Bot for Bing
 // @author       Kochetov Egor 066
 // @match        https://www.bing.com/*
 // @match        https://auto.ru/*
+// @match        https://motoreforma.com/*
+// @match        https://ironduke.ru/*
 // @grant        none
 // ==/UserScript==
 
 let bingInput = document.getElementsByName("q")[0];
 let bingBtn = document.getElementsByClassName("search")[0];
 let links = document.links;
-let keywords = ["Купить машину", "BMW e46 купить", "Раритетные иномарки в Москве", "Купить мотоцикл Harley-Davidson", "Купить мотоцикл типа cafe racer"];
+let sites = {
+  "auto.ru": ["Купить машину", "BMW e46 купить", "Раритетные иномарки в Москве", "Купить мотоцикл Harley-Davidson", "Купить мотоцикл типа cafe racer"],
+  "motoreforma.com": ["Аксессуары для тюнинга мототехники", "Запчасти для мототехники", "Тюнинг Maverick X3"],
+  "ironduke.ru": ["ТО автомобилей в Измайлово", "Мастерская Iron Duke", "Замена датчиков давления автомобиля"],
+}
+let sitesKeys = Object.keys(sites);
+let site = sitesKeys[getRandom(0, sitesKeys.length)];
+let keywords = sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
-let mouseClick = new MouseEvent("click");
 
 //Работаем на главной странице
 if (bingBtn !== undefined) {
@@ -28,13 +36,13 @@ if (bingBtn !== undefined) {
     bingInput.click();
     bingInput.value += keyword[i];
     i++;
-    if(i == keyword.length) {
+    if (i == keyword.length) {
       clearInterval(timerId);
       bingBtn.click();
     }
   }, getRandom(300, 500))
   //Работаем на целевом сайте
-  } else if (location.hostname == "auto.ru") {
+  } else if (location.hostname == site) {
     let linkIndex = getRandom(0, links.length);
     let localLink = links[linkIndex];
     setInterval(() => {
@@ -44,7 +52,7 @@ if (bingBtn !== undefined) {
       }
 
       if (links.length == 0) {
-        location.href = "https://auto.ru/";
+        location.href = site;
       }
 
       if (localLink.href.includes("auto.ru")) {
@@ -58,7 +66,7 @@ else if (document.querySelector(".b_scopebar") !== null) {
   //привязка к scopebar позволяет избежать многих ошибок с дальнейшей работой бота на выбранном ресурсе
   let nextPage = true;
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("auto.ru") != -1) {
+    if (links[i].href.indexOf(site) != -1) {
       let link = links[i];
       let nextPage = false;
       console.log("Найден результат " + link);
