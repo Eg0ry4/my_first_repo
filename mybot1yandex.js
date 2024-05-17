c7d04b7182ff705e9d460e3b258ebb2642d59bf9
Eg0ry4 <166389917+Eg0ry4@users.noreply.github.com>
2024-05-17 23:34:02 +0300

    Версия яндекс-бота 1.03 для задания 16*
    Добавлены Cookie + другие целевые сайты

--- mybot1yandex.js
+++ mybot1yandex.js
@@ -1,20 +1,38 @@
 // ==UserScript==
 // @name         My first Bot Yandex
 // @namespace    http://http://inoue.p-host.in
-// @version      1.02
+// @version      1.03
 // @description  Bot for Yandex
 // @author       Kochetov Egor 066
 // @match        https://ya.ru/*
 // @match        https://auto.ru/*
+// @match        https://motoreforma.com/*
+// @match        https://ironduke.ru/*
 // @grant        none
 // ==/UserScript==
 
 let yaInput = document.getElementsByName("text")[0];
 let yaBtn = document.getElementsByClassName("search3__button")[0];
 let links = document.links;
-let keywords = ["Купить машину", "BMW e46 купить", "Раритетные иномарки в Москве", "Купить мотоцикл Harley-Davidson", "Купить мотоцикл типа cafe racer"];
+let sites = {
+  "auto.ru": ["Купить машину", "BMW e46 купить", "Раритетные иномарки в Москве", "Купить мотоцикл Harley-Davidson", "Купить мотоцикл типа cafe racer"],
+  "motoreforma.com": ["Прошивки для CAN-AM BRP", "вариатор CVTech CAN-AM", "Тюнинг Maverick X3"],
+  "ironduke.ru": ["ТО автомобилей в Измайлово", "Мастерская Iron Duke", "Замена датчиков давления автомобиля"],
+}
+let sitesKeys = Object.keys(sites);
+let site = sitesKeys[getRandom(0, sitesKeys.length)];
+let keywords = sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
 let mouseClick = new MouseEvent("click");
+
+if (yaBtn !== undefined) {
+  document.cookie = `site=${site}`;
+} else if (location.hostname == "ya.ru") {
+  site = getCookie("site");
+} else {
+  site = location.hostname;
+}
+
 
 if (yaBtn !== undefined) {
   let i = 0;
@@ -28,20 +46,20 @@ if (yaBtn !== undefined) {
     }
   }, getRandom(200, 400))
   //Работа на целевом сайте
-  } else if (location.hostname == "auto.ru") {
-    let linkIndex = getRandom(0, links.length);
-    let localLink = links[linkIndex];
+  } else if (location.hostname == site) {
     setInterval(() => {
+      let linkIndex = getRandom(0, links.length);
+      let localLink = links[linkIndex];
 
       if (getRandom(0, 101) > 50) {
-        location.href = "https://ya.ru/";
+        location.href = "ya.ru";
       }
 
       if (links.length == 0) {
-        location.href = "https://auto.ru/";
+        location.href = site;
       }
 
-      if (localLink.href.includes("auto.ru")) {
+      if (localLink.href.includes(site)) {
         localLink.click();
       }
     }, getRandom(3000, 5000))
@@ -51,7 +69,7 @@ if (yaBtn !== undefined) {
 else if ((document.querySelector(".HeaderDesktopNavigation") !== null)) {
   let nextPage = true;
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("auto.ru") != -1) {
+    if (links[i].href.indexOf(site) != -1) {
       let link = links[i];
       let nextPage = false;
       console.log("Найден результат " + link);
@@ -81,4 +99,10 @@ else if ((document.querySelector(".HeaderDesktopNavigation") !== null)) {
 }
 function getRandom(min,max) {
   return Math.floor(Math.random() * (max - min) + min)
+}
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
